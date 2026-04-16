ca08f5cea050b2ce15f7345a52513ab0b70d58af
Dabson10 <dabson753@gmail.com>
2026-04-16 16:11:34 -0600

    Se creo el proyecto y el repo

---
+++ src/main/java/org/github/dabson10/gamevault/GameVaultApplication.java
@@ -0,0 +1,13 @@
+package org.github.dabson10.gamevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameVaultApplication.class, args);
+    }
+
+}
